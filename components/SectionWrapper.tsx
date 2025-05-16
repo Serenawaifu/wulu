4cbd3988436c0bfa6c86ff32b26f68c30725ecbe
Sparkie <78498155+Serenawaifu@users.noreply.github.com>
2025-05-16 23:52:51 +0530

    Create SectionWrapper.tsx

---
+++ components/SectionWrapper.tsx
@@ -0,0 +1,13 @@
+export default function SectionWrapper({
+  children,
+  className = '',
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className={`mx-auto max-w-7xl px-6 ${className}`}>
+      {children}
+    </div>
+  );
+}
